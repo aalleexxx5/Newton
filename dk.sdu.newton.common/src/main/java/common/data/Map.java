f7ff31d1bd98b34c8c12cbace41a29cb1e7f8976
DaeLee96 <Caodaily@gmail.com>
2018-03-20 13:04:21 +0100

    Added Connections, Map, Room

---
+++ dk.sdu.newton.common/src/main/java/common/data/Map.java
@@ -0,0 +1,4 @@
+package common.data;
+
+public class Map {
+}
---
+++ dk.sdu.newton.common/src/main/java/common/data/Room.java
@@ -0,0 +1,11 @@
+package common.data;
+
+public class Room {
+    private String name;
+    private float width;
+    private float heigth;
+    private float yPosition;
+    private float xPosition;
+
+
+}
