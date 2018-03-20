a1552ec2286866bbfde02cef78f27b45d6ff2afc
DaeLee96 <Caodaily@gmail.com>
2018-03-20 13:53:12 +0100

    BOXES!

--- dk.sdu.newton.common/src/main/java/common/data/Room.java
+++ dk.sdu.newton.common/src/main/java/common/data/Room.java
@@ -1,11 +1,45 @@
 package common.data;
 
-public class Room {
+public class Room{
     private String name;
-    private float width;
-    private float heigth;
-    private float yPosition;
-    private float xPosition;
 
+    public Sprite createBigNWBox() {
+        Sprite sprite = new Sprite("bigNWBox", -550, 540, 500, 50);
+        return sprite;
+    }
 
+    public Sprite createBigNEBox() {
+        Sprite sprite = new Sprite("bigNEBox", 50, 540, 500, 50);
+        return sprite;
+    }
+
+    public Sprite createBigSWBox() {
+        Sprite sprite = new Sprite("bigSWBox", -550, -490, 500, 50);
+        return sprite;
+    }
+
+    public Sprite createBigSEBox() {
+        Sprite sprite = new Sprite("bigSEBox", 50, -490, 500, 50);
+        return sprite;
+    }
+
+    public Sprite createSmallNWBox() {
+        Sprite sprite = new Sprite("smallNWBox", -600, 540, 50, 490);
+        return sprite;
+    }
+
+    public Sprite createSmallNEBox() {
+        Sprite sprite = new Sprite("smallNEBox", 550, 540, 50, 490);
+        return sprite;
+    }
+
+    public Sprite createSmallSWBox() {
+        Sprite sprite = new Sprite("smallSWBox", -600, -50, 50, 490);
+        return sprite;
+    }
+
+    public Sprite createSmallSEBox() {
+        Sprite sprite = new Sprite("smallSEBox", 550, -50, 50, 490);
+        return sprite;
+    }
 }
