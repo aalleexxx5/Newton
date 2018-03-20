b861b128c31266711118f57de52bb89e9a3b7a9d
ximias <alexx4387@gmail.com>
2018-03-20 12:10:48 +0100

    Implemented collidable.

--- dk.sdu.newton.collision/src/main/java/dk/sdu/newton/collision/internal/CollisionChecker.java
+++ dk.sdu.newton.collision/src/main/java/dk/sdu/newton/collision/internal/CollisionChecker.java
@@ -2,11 +2,40 @@ package dk.sdu.newton.collision.internal;
 
 import common.data.GameState;
 import common.data.Registrator;
+import common.services.Collidable;
 import common.services.Updatable;
 
+import java.util.ArrayList;
+
 public class CollisionChecker implements Updatable{
+	/**
+	 * Called after each frame. Determines if all collidables are colliding.
+	 * @param state the gameState containing the collidables.
+	 */
 	@Override
 	public void update(GameState state) {
-	
+		ArrayList<Collidable> collidables = state.getEntitiesByInterface(Collidable.class);
+		for (int i = 0; i < collidables.size(); i++) {
+			for (int j = i+1; j < collidables.size(); j++) {
+				if (doesCollide(collidables.get(i), collidables.get(j))){
+					collidables.get(i).collidesWith(collidables.get(j));
+					collidables.get(j).collidesWith(collidables.get(i));
+				}
+			}
+		}
 	}
+	
+	/**
+	 * Used to determine if two collidables is colliding.
+	 * @param a A collidable.
+	 * @param b A different collidable.
+	 * @return true, if the two collidables are overlapping.
+	 */
+	private boolean doesCollide(Collidable a, Collidable b) {
+		float[] aBounds = a.getBounds();
+		float[] bBounds = b.getBounds();
+		return !(aBounds[0] > bBounds[0] + bBounds[2]) &&  // a.x < b.x+b.width
+				!(bBounds[0] > aBounds[0] + aBounds[2]) && // b.x < a.x+a.width
+				!(aBounds[1] > bBounds[1] + bBounds[3]) && // a.y < b.y+b.height
+				!(bBounds[1] > aBounds[1] + aBounds[3]); } // b.y < a.y+a.height
 }
--- dk.sdu.newton.common/src/main/java/common/services/Collidable.java
+++ dk.sdu.newton.common/src/main/java/common/services/Collidable.java
@@ -1,7 +1,22 @@
 package common.services;
 
 public interface Collidable {
+	/**
+	 * Used by other collidables to determine the hostility of this Collidable.
+	 * @return the hostility of the collidable.
+	 */
     public Enum getHostility();
-    public void collidesWith();
+	
+	/**
+	 * Called when a collision has been detected.
+	 * @param source the offending object.
+	 */
+	public void collidesWith(Collidable source);
+	
+	/**
+	 * Used by collision to get the bounding box for collision checking.
+	 * @return an array containing the x,y,width,height of the Collidable.
+	 */
+	public float[] getBounds();
 
 }
