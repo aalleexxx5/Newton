b63791a1ad30dce3f47e93761595b8cb2c22ace1
ximias <alexx4387@gmail.com>
2018-04-10 12:37:27 +0200

    Fixed einsteinWeapon

--- dk.sdu.newton.einstein/src/main/java/dk/sdu/newton/einstein/EinsteinWeapon.java
+++ dk.sdu.newton.einstein/src/main/java/dk/sdu/newton/einstein/EinsteinWeapon.java
@@ -1,18 +1,26 @@
 package dk.sdu.newton.einstein;
 
+import common.data.GameState;
 import common.data.Weapon;
 
 public class EinsteinWeapon extends Weapon{
 
 	private Einstein einstein;
-
+	
 	@Override
-	public void onEquip() {
-	}
-
-	private void fire(){
+	public void shoot(GameState state) {
 		float x = einstein.getBounds()[0];
 		float y = einstein.getBounds()[1];
 		EinsteinBullet einsteinBullet = new EinsteinBullet(x, y, 10, 10);
+	}
+	
+	@Override
+	public void onEquip(GameState state) {
+	
+	}
+	
+	@Override
+	public void onUnEquip(GameState state) {
+	
 	}
 }
